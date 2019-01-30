08edf14accd0f6212cdcf54c57b0d69f3f402c3b
pranav1234 <pranav0492@gmail.com>
2019-01-30 11:56:32 +0530

    add selectors to ping

---
+++ src/features/ping/index.ts
@@ -0,0 +1,10 @@
+import pingReducer from "./reducer";
+import * as pingConstants from './constants';
+import * as pingSelectors from './selectors';
+import * as pingActions from './actions';
+export {
+     pingReducer,
+     pingConstants,
+     pingActions,
+     pingSelectors
+};
---
+++ src/features/ping/selectors.ts
@@ -0,0 +1,1 @@
+export const getIsPing = (state: any) => state.ping.isPinging;
